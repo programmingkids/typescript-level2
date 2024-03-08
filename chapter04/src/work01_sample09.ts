91de8fd0ae30f18000a70682ceaeb7f14b14f7ca
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-03-08 10:08:54 +0000

    fix chapter04 for main

--- chapter04/src/work01_sample09.ts
+++ chapter04/src/work01_sample09.ts
@@ -1,9 +1,9 @@
 export {};
 
 // Arrayクラスを利用したstringの配列
-const fruits: Array<string> = new Array('apple', 'orange', 'melon');
+const fruits: Array<string> = new Array<string>('apple', 'orange', 'melon');
 console.log(fruits);
 
 // Arrayクラスを利用したnumberの配列
-const numbers: Array<number> = new Array(10, 20, 30);
+const numbers: Array<number> = new Array<number>(10, 20, 30);
 console.log(numbers);
